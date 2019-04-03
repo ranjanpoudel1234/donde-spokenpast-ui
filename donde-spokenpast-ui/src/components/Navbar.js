1c85bcb43ada328d676612a6d786d50e25070a4a
Aaron Moorman <moorma.aar@gmail.com>
2019-04-02 20:00:06 -0500

    Drawer for Navbar
    Still need to implement click away to close

--- donde-spokenpast-ui/src/components/Navbar.js
+++ donde-spokenpast-ui/src/components/Navbar.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import NavDrawer from './buttons';
 import Toolbar from '@material-ui/core/Toolbar';
 
+
+
+
 const NavBar = () => {
+  
     return(
         <div>
         <AppBar position="static">
             <Toolbar>
+                <NavDrawer />
                 SpokenPast
             </Toolbar>
         </AppBar>
         </div>
     )
 }
+
+
 export default NavBar;
